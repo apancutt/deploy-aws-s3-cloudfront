b3b96ed0a3f8dc6552219c4b4cf2ec7d1595c384
Adam Pancutt <adam@pancutt.com>
2018-03-01 13:26:20 +0000

    Import

---
+++ README.md
@@ -0,0 +1,86 @@
+# deploy-aws-s3-cloudfront
+
+Syncs a local directory to an AWS S3 bucket, optionally invalidating affected CloudFront paths.
+
+## Authentication
+
+This packages uses the [AWS SDK for Node.js](https://aws.amazon.com/sdk-for-node-js/) and defers authentication to the SDK.
+
+If you are relying on credentials stored in `~/.aws/credentials` you can use the `--profile` option to specify a named profile, if required.
+
+## Installation
+
+    npm install --save deploy-aws-s3-cloudfront
+
+## Usage
+
+    deploy-aws-s3-cloudfront [options]
+    
+    Syncs a local directory to an AWS S3 bucket, optionally invalidating affected
+    CloudFront paths.
+    
+    Options:
+      --help             Show help                                         [boolean]
+      --version          Show version number                               [boolean]
+      --bucket           AWS S3 bucket name to deploy to         [string] [required]
+      --distribution     AWS CloudFront distribution ID to invalidate       [string]
+      --source           Path to local directory to sync from
+                                                  [string] [required] [default: "."]
+      --destination      Path to remote directory to sync to
+                                                  [string] [required] [default: "/"]
+      --profile          AWS profile to use as named in ~/.aws/credentials  [string]
+      --non-interactive  Do not prompt for confirmation   [boolean] [default: false]
+
+### Installing a `run-script` alias (optional)
+
+Add a `deploy` script alias to your `package.json` file:
+
+    {
+      ...
+      "scripts": {
+        ...
+        "deploy": "deploy-aws-s3-cloudfront [options]"
+      }
+    }
+
+Run `npm run build` to build then `npm run deploy` to deploy.
+
+If you need to pass user-level options that you don't wan't committed into `package.json`, the you can provide these options at call-time, e.g. `npm run deploy -- --profile <profile>`.
+
+### Configuring for React apps built with `create-react-app`
+
+Set the `--source` option to `/.build/`:
+
+    {
+      ...
+      "scripts": {
+        ...
+        "deploy": "deploy-aws-s3-cloudfront --source ./build/ [options]"
+      }
+    }
+
+Then simply run `npm run build` then `npm run deploy` to deploy latest build.
+
+If you prefer to always run a build before deployment:
+
+    {
+      ...
+      "scripts": {
+        ...
+        "deploy": "npm run build && deploy-aws-s3-cloudfront --source ./build/ [options]"
+      }
+    }
+
+### Alternatives (and why this package exists!)
+
+* [AWS S3 Sync](https://docs.aws.amazon.com/cli/latest/reference/s3/sync.html) (bundled with [AWS CLI](https://aws.amazon.com/cli/))
+
+  The `aws s3 sync` command uses the modification time to identify modified assets. This doesn't work well when building a project often involves regenerating files with fresh timestamps but identical content.
+
+  This package will instead perform a checksum comparison to minimise the deployment payload. The MD5 checksum will be computed against all files then compared against the ETag of the corresponding remote objects.
+
+* [react-deploy-s3](https://www.npmjs.com/package/react-deploy-s3) (NPM package)
+
+  For React apps, the `react-deploy-s3` provides similar behaviour to this package. However, `react-deploy-s3` expects your AWS credentials to be passed in as command arguments and requires additional configuration to get set up. In contrast, this package defers authentication to the AWS SDK and therefore supports multiple authentication strategies (e.g. IAM roles, environment variables and profiles).
+
+  Additionally, `react-deploy-s3` will purge everything from your S3 bucket before re-uploading the entire build directory. Here, however, deployments are incremental resulting in a smaller payload and minimal interruption. Likewise, this package will perform a more efficient CloudFront purge by executing an invalidation on the affected paths only, as opposed to a site-wide refresh as performed by `react-deploy-s3`.
---
+++ bin/deploy-aws-s3-cloudfront.js
@@ -0,0 +1,403 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2018-present, Adam Pancutt
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const aws = require('aws-sdk');
+const colors = require('colors/safe');
+const fs = require('fs');
+const md5File = require('md5-file')
+const mimeTypes = require('mime-types')
+const path = require('path');
+const prettyBytes = require('pretty-bytes');
+const promptConfirm = require('prompt-confirm');
+const yargs = require('yargs');
+
+const argv = yargs
+  .usage('$0 [options]', 'Syncs a local directory to an AWS S3 bucket, optionally invalidating affected CloudFront paths.')
+  .option('bucket', {
+    type: 'string',
+    demand: true,
+    describe: 'AWS S3 bucket name to deploy to',
+  })
+  .option('distribution', {
+    type: 'string',
+    describe: 'AWS CloudFront distribution ID to invalidate',
+  })
+  .option('source', {
+    type: 'string',
+    demand: true,
+    describe: 'Path to local directory to sync from',
+    default: '.',
+  })
+  .option('destination', {
+    type: 'string',
+    demand: true,
+    describe: 'Path to remote directory to sync to',
+    default: '/',
+  })
+  .option('profile', {
+    type: 'string',
+    describe: 'AWS profile to use as named in ~/.aws/credentials',
+  })
+  .option('non-interactive', {
+    type: 'boolean',
+    describe: 'Do not prompt for confirmation',
+    default: false,
+  })
+  .argv;
+
+if (!path.isAbsolute(argv.source)) {
+  argv.source = path.resolve(argv.source);
+}
+argv.source = argv.source.replace(/\/$/g, '') + '/';
+
+argv.destination = argv.destination.replace(/^\/|\/$/g, '');
+if (argv.destination) {
+  argv.destination += '/';
+}
+
+if (argv.profile) {
+  aws.config.credentials = new aws.SharedIniFileCredentials({
+    profile: argv.profile,
+  });
+}
+
+colors.setTheme({
+  debug: 'cyan',
+  info: 'green',
+  warn: 'yellow',
+  error: 'red',
+});
+
+const s3 = new aws.S3();
+const cloudfront = new aws.CloudFront();
+
+fetch()
+  .then((fetched) => {
+
+    changes(fetched)
+      .then((changed) => {
+
+        if (!changed.added.length && !changed.modified.length && !changed.deleted.length) {
+          console.log(colors.info('No changes detected'));
+          process.exit();
+        }
+
+        const proceed = function() {
+          deploy(changed.added.concat(changed.modified), changed.deleted)
+            .then((deployed) => {
+
+              invalidate(deployed.uploaded.concat(deployed.deleted))
+                .then((invalidated) => {
+
+                  console.log(colors.info('Deployment complete'));
+
+                })
+                .catch((err) => {
+                  handleError(err, 'Invalidation failure (' + err.message + ')');
+                });
+
+            })
+            .catch((err) => {
+              handleError(err, 'Deployment failure (' + err.message + ')');
+            });
+        };
+
+        if (argv.nonInteractive) {
+          proceed();
+        } else {
+          new promptConfirm('Continue deployment?').ask((answer) => {
+            if (!answer) {
+              console.log(colors.warn('Deployment aborted'));
+              process.exit();
+            }
+            proceed();
+          });
+        }
+
+      })
+      .catch((err) => {
+        handleError(err, 'Change detection failure (' + err.message + ')');
+      });
+
+  })
+  .catch((err) => {
+    handleError(err, 'Fetch objects failure (' + err.message + ')');
+  });
+
+function fetch() {
+  return new Promise((resolve, reject) => {
+
+    const results = {
+      local: undefined,
+      remote: undefined,
+    }
+
+    function local() {
+      return new Promise((resolve, reject) => {
+
+        const results = {};
+
+        function fetch(dir) {
+          return fs.statSync(dir).isDirectory()
+            ? Array.prototype.concat(...fs.readdirSync(dir).map(file => fetch(path.join(dir, file))))
+            : dir;
+        }
+
+        console.log(colors.debug('Fetching local objects in ' + colors.bold(argv.source) + '...'));
+
+        fetch(argv.source).forEach((result) => {
+          results[result.replace(argv.source, '')] = md5File.sync(result);
+        });
+
+        resolve(results);
+
+      });
+    }
+
+    function remote() {
+      return new Promise((resolve, reject) => {
+
+        const params = {
+          Bucket: argv.bucket,
+          Prefix: argv.destination,
+        };
+
+        const results = {};
+
+        const fetch = () => {
+
+          s3.listObjectsV2(params, (err, data) => {
+
+            if (err) {
+              return reject(err);
+            }
+
+            data.Contents.forEach((content) => {
+              results[content.Key] = content.ETag.replace(/"/g, '');
+            });
+
+            if (data.IsTruncated) {
+              params.ContinuationToken = data.NextContinuationToken;
+              return fetch();
+            }
+
+            resolve(results);
+
+          });
+        };
+
+        console.log(colors.debug('Fetching remote objects in ' + colors.bold(params.Bucket + (params.Prefix ? '/' + params.Prefix : '')) + '...'));
+
+        fetch();
+
+      });
+    }
+
+    local()
+      .then((fetched) => {
+        results.local = fetched;
+        if (results.remote) {
+          resolve(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+
+    remote()
+      .then((fetched) => {
+        results.remote = fetched;
+        if (results.local) {
+          resolve(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+
+  });
+}
+
+function changes(fetched) {
+  return new Promise((resolve, reject) => {
+
+    const results = {
+      added: [],
+      modified: [],
+      deleted: [],
+    };
+
+    console.log(colors.debug('Checking for modifications...'));
+
+    Object.keys(fetched.local).forEach((key) => {
+      if (!fetched.remote.hasOwnProperty(key)) {
+        results.added.push(key);
+        console.log(colors.green.bold('A ') + key);
+      } else if (fetched.local[key] !== fetched.remote[key]) {
+        results.modified.push(key);
+        console.log(colors.yellow.bold('M ') + key);
+      }
+    });
+
+    Object.keys(fetched.remote).forEach((key) => {
+      if (!fetched.local.hasOwnProperty(key)) {
+        results.deleted.push(key);
+        console.log(colors.red.bold('D ') + key);
+      }
+    });
+
+    resolve(results);
+
+  });
+}
+
+function deploy(uploads, deletes) {
+  return new Promise((resolve, reject) => {
+
+    function uploadObjects() {
+      return new Promise((resolve, reject) => {
+
+        const uploaded = [];
+
+        const defaults = {
+          Bucket: argv.bucket,
+          ACL: 'public-read',
+        };
+
+        try {
+
+          uploads.forEach((key) => {
+
+            const path = argv.source + key;
+            const stats = fs.statSync(path);
+            const stream = fs.createReadStream(path);
+
+            stream.on('error', function(err) {
+              throw err;
+            });
+
+            let params = Object.assign({
+              Body: stream,
+              Key: argv.destination + key,
+              ContentType: mimeTypes.lookup(path) || 'application/octet-stream',
+              ContentLength: stats.size,
+            }, defaults);
+
+            console.log(colors.info('Uploading ' + colors.bold(params.Key) + ' (' + prettyBytes(params.ContentLength)  + ') as ' + colors.bold(params.ContentType) + ' to ' + colors.bold(params.Bucket) + '...'));
+
+            s3.upload(params, function(err, data) {
+              if (err) {
+                throw err;
+              }
+              uploaded.push(params.Key);
+              if (uploaded.length === uploads.length) {
+                resolve(uploaded);
+              }
+            });
+
+          });
+
+        } catch (error) {
+          reject(error);
+        }
+
+      });
+    }
+
+    function deleteObjects() {
+      return new Promise((resolve, reject) => {
+
+        const deleted = [];
+
+        if (!deletes.length) {
+          return resolve(deleted);
+        }
+
+        const params = {
+          Bucket: argv.bucket,
+          Delete: {
+            Objects: [],
+          },
+        };
+
+        deletes.forEach((key) => {
+          key = argv.destination + key;
+          console.log(colors.warn('Deleting ' + colors.bold(key) + ' from ' + colors.bold(argv.bucket) + '...'));
+          params.Delete.Objects.push({Key: key});
+          deleted.push(key);
+        });
+
+        s3.deleteObjects(params, function(err, data) {
+          err ? reject(err) : resolve(deleted);
+        });
+
+      });
+    }
+
+    uploadObjects()
+      .then((uploaded) => {
+
+        deleteObjects()
+          .then((deleted) => {
+
+            resolve({
+              uploaded,
+              deleted,
+            });
+
+          })
+          .catch((err) => {
+            reject(err);
+          });
+
+      })
+      .catch((err) => {
+        reject(err);
+      });
+
+  });
+}
+
+function invalidate(invalidations) {
+  return new Promise((resolve, reject) => {
+
+    if (!argv.distribution) {
+      resolve([]);
+    }
+
+    const invalidated = [];
+    let path;
+
+    invalidations.forEach((key) => {
+      path = '/' + key;
+      invalidated.push(path);
+      console.log(colors.info('Invalidating ' + colors.bold(path) + ' on CloudFront distribution ' + colors.bold(argv.distribution) + '...'));
+    });
+
+    const params = {
+      DistributionId: argv.distribution,
+      InvalidationBatch: {
+        CallerReference: Math.floor(Date.now() / 1000).toString(),
+        Paths: {
+          Quantity: invalidated.length,
+          Items: invalidated,
+        },
+      },
+    };
+
+    cloudfront.createInvalidation(params, (err, data) => {
+      err ? reject(err) : resolve(invalidated);
+    });
+
+  });
+}
+
+function handleError(err, message) {
+  console.error(colors.red(colors.bold('ERROR ') + message || err.message));
+  process.exit(1);
+}
