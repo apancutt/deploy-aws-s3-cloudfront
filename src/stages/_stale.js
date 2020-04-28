75eb4304b08b18c42e344b9580187196b4366cb5
Adam Pancutt <adam@pancutt.com>
2020-04-28 07:51:51 +0200

    WIP

--- src/stages/_stale.js
+++
@@ -1,9 +0,0 @@
-module.exports = (uploaded, deleted, options) => Promise.resolve(
-  options.distribution
-    ? (
-      options['invalidation-paths'].length
-        ? options['invalidation-paths']
-        : Object.entries(uploaded).concat(options['soft-delete'] ? [] : Object.entries(deleted)).map(([ , params ]) => params.destination)
-    )
-    : []
-);
--- src/stages/_summarize.js
+++
@@ -1,9 +0,0 @@
-const { debug, info } = require('./log');
-
-module.exports = (uploaded, deleted, invalidated, options) => {
-  info('Deployment complete')
-  debug(`Uploaded: ${Object.keys(uploaded).length}`);
-  debug(`${options['soft-delete'] ? 'Soft Deleted' : 'Deleted'}: ${Object.keys(deleted).length}`);
-  debug(`Invalidated: ${invalidated.length}`);
-  return Promise.resolve();
-};
